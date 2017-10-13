fcdee396af7d6171d420b2ba606f17706a675b06
Spencer Smith <WankFerret@Stevens-MacBook.attlocal.net>
2017-10-13 08:44:22 -0500

    working

---
+++ my-first-react-app/src/Child.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react'
+
+class Child extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username:  '',
+            password: ''
+        }
+    this.handleUpdateUsername = this.handleUpdateUsername.bind(this)
+    this.handleUpdatePassword = this.handleUpdatePassword.bind(this)
+    this.loginUser = this.loginUser.bind(this)
+
+
+    }
+    handleUpdateUsername(event){
+        this.setState({
+            username: event.target.value
+        })
+    }
+    handleUpdatePassword(event){
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    loginUser() {
+        console.log(`Logging in ${this.state.username} with password ${this.state.password}!`)
+    }
+
+
+    render(){
+         return (
+            <div>
+                <input
+                onChange={this.handleUpdateUsername}
+                placeholder="UserName"/>
+                <input
+                onChange={this.handleUpdatePassword}
+                placeholder="Password"/>
+                <button
+                onClick={this.loginUser}>
+                    Login</button>
+            </div>
+
+        )
+    }
+}
+
+export default Child
