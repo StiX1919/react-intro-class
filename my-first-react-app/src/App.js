43dba8882bb01267fe5d48c0498a7c85204f2270
Spencer Smith <WankFerret@Stevens-MacBook.local>
2017-10-13 15:15:43 -0500

    its a calqlater

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Calculator from './components/Calculator/Calculator.js';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
